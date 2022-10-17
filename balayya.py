fb0a0175c38f5825f10418d2adbef2cb2b0b189f
Duleep <duleep.daniel@gmail.com>
2022-10-17 17:40:13 +0530

    Feat: switch to Streams

--- balayya.py
+++ balayya.py
@@ -1,6 +1,4 @@
-import praw
-import time
-import random
+import praw, random
 
 reddit = praw.Reddit(
     client_id="XXX-YYY-ZZZZ",
@@ -8,29 +6,26 @@ reddit = praw.Reddit(
     user_agent="Comment Extraction (by u/balayya-bot)", # something Unique
     username="balayya-bot",
     password="XXX-YYY-ZZZZ",
-)
+) # TODO: Reddit App Credentials
+
+comment_header = "> Ignore me, I am a *Safe* **Bot**\n\n"
 
 balayya_slogans = [
-  "Coca-Cola Pepsi…Balayya Babu Sexy - Balayya Veera Abhimani",
-  "Annam lo Perugu Ledhu Balayya Babu ki Thirugu Ledu - Balayya Veera Abhimani",
-  "Palakura Pappu Balayya Babu Nippu - Balayya Veera Abhimani",
-  "Vadevadu Videvadu Ma Balayya Babu ki Addu Evadu - Balayya Veera Abhimani",
-  "Jill Jill Jiga Maa Balayya Babu sega - Balayya Veera Abhimani",
-  "Varshakalam lo Current Kotha Ammaila Gundello Balayya Babu Motha - Balayya Veera Abhimani",
-  "Adavilo pudithe simham la puttali Maro janmantuunte balayya abhimanila puttali - Balayya Veera Abhimani",
-  "Smoking is injurious – Balayya babu dangerous - Balayya Veera Abhimani",
-  "Water quarter – balayya babu dictator - Balayya Veera Abhimani",
+  "Coca-Cola Pepsi…Balayya Babu Sexy",
+  "Annam lo Perugu Ledhu Balayya Babu ki Thirugu Ledu",
+  "Palakura Pappu Balayya Babu Nippu",
+  "Vadevadu Videvadu Ma Balayya Babu ki Addu Evadu",
+  "Jill Jill Jiga Maa Balayya Babu sega",
+  "Varshakalam lo Current Kotha Ammaila Gundello Balayya Babu Motha",
+  "Adavilo pudithe simham la puttali Maro janmantuunte balayya abhimanila puttali",
+  "Smoking is injurious – Balayya babu dangerous",
+  "Water quarter – balayya babu dictator",
 ]
 
-subs = ["Ni_Bondha", "hyderabad"]
+sub_reddit = reddit.subreddit("#sub_reddit") #TODO: Enter your public Subreddit
 
-for sub in subs:
-  sub_reddit = reddit.subreddit(sub)
-
-  for post in sub_reddit.hot(limit=50):
-    for comment in post.comments:
-      if ' balayya ' in comment.body.lower():
-        random_index = random.randint(0, len(balayya_slogans) - 1)
-        comment.reply(balayya_slogans[random_index])
-        print("--- Done ---")
-        time.sleep(600) # Reddit Bot Limits - Refer: https://www.reddit.com/wiki/bottiquette/ 
+for comment in sub_reddit.stream.comments(skip_existing=True):
+  if 'balayya' in comment.body.lower() and not comment.author ==  reddit.user.me():
+    random_index = random.randint(0, len(balayya_slogans) - 1)
+    comment.reply(comment_header + balayya_slogans[random_index])
+    print('---- commented on ' + comment.id + ' by ' + comment.author.name + ' ----')
